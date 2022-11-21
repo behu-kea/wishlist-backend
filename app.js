175f4e4e8c004f9156481518d3628dcf188c3b8f
Benjamin Hughes <behu@kea.dk>
2022-11-21 13:19:27 +0100

    asd

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 require('dotenv');
 const mysqlConnection = require("./database");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Your github page origin has to be written EXACTLY like this! https://behu-kea.github.io
 const URL_FOR_FRONTEND = "YOUR_GITHUB_PAGE_ORIGIN_HERE";
@@ -92,54 +92,8 @@ app.get('/database-sql-injection/:description', (req, res) => {
             }
         }
     );
-})
-
+});
 // DO NOT MAKE YOUR CODE VULNERABLE TO SQL INJECTION!!!
-
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-// READ: read all users from users
-app.get("/users", (req, res) => {
-    return res.json(users);
-});
-
-// READ: get user by id
-app.get("/users/:id", (req, res) => {
-    const id = req.params.id;
-    const user = users.find(item => item.id == id);
-    return res.json(user);
-});
-
-// CREATE: create new user and add to users
-app.post("/users", (req, res) => {
-    let newUser = req.body;
-    const timestamp = Date.now(); // dummy generated user id
-    newUser.id = timestamp;
-    users.push(newUser);
-    return res.json(users);
-});
-
-// UPDATE: update existing user
-app.put("/users/:id", (req, res) => {
-    const id = req.params.id;
-    const userData = req.body;
-    let user = users.find(item => item.id == id);
-    user.name = userData.name;
-    user.title = userData.title;
-    user.mail = userData.mail;
-    user.image = userData.image;
-    return res.json(users);
-});
-
-// DELETE: delete user
-app.delete("/users/:id", (req, res) => {
-    const id = req.params.id;
-    users = users.filter(item => item.id != id);
-    return res.json(users);
-});
 
 app.listen(port, () => {
     console.log(`Node.js REST API listening at http://localhost:${port}`);
